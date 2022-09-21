b12915f1a8d0fc3bdc2ddf855bf99d8f66db9dcd
sunjae <sunjae@iseonjaes-mbp.dhcp.wustl.edu>
2022-09-21 17:14:22 -0500

    :(

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,17 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please choose a number.");
+		int[]numbers = new int[in.nextInt()];
+		
+	for(int value : numbers);
+	
+	}
+
+}
